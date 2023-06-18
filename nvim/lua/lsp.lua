c9bf18af799c47c8289481a870202d67decd0b9c
vinit payal <vinitpayal@gmail.com>
2023-06-18 18:26:33 +0530

    lsp changes

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,39 +1,12 @@
-local cmp = require 'cmp'
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
 
 require("luasnip.loaders.from_vscode").load({ include = { "python", "javascript", "typescript" } })
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'luasnip' }
-  }, {
-    { name = 'buffer' },
-  })
-})
-
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-require("mason-lspconfig").setup {
-  ensure_installed = {
+local languageServers = {
     "lua_ls",
     "tsserver",
     "pylsp",
@@ -43,26 +16,101 @@ require("mason-lspconfig").setup {
     "sqlls",
     "bashls",
     "jsonls"
-    -- "quick_lint_js"
+}
+
+
+require("mason-lspconfig").setup {
+  ensure_installed = languageServers
+}
+
+-- Enable some language servers with the additional completion capabilities offered by nvim-cmp
+for _, lsp in ipairs(languageServers) do
+  lspconfig[lsp].setup {
+    capabilities = capabilities,
+  }
+end
+
+-- luasnip setup
+local luasnip = require 'luasnip'
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
+    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
+    -- C-b (back) C-f (forward) for snippet placeholder navigation.
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
   },
 }
 
-require("lspconfig").lua_ls.setup { capabilities = capabilities }
-require("lspconfig").tsserver.setup { capabilities = capabilities }
---require("lspconfig").tsserver.setup {
---  capabilities = capabilities,
---  on_attach = function(client)
---    client.server_capabilities.documentFormattingProvider = false
---    client.server_capabilities.documentRangeFormattingProvider = false
---  end
---}
-require("lspconfig").pylsp.setup { capabilities = capabilities }
-require("lspconfig").dockerls.setup { capabilities = capablities }
-require("lspconfig").docker_compose_language_service.setup { capabilities = capablities }
-require("lspconfig").marksman.setup { capabilities = capablities }
-require("lspconfig").sqlls.setup { capabilities = capablities }
-require("lspconfig").bashls.setup { capabilities = capablities }
-require("lspconfig").jsonls.setup { capabilities = capablities }
+--cmp.setup({
+--  snippet = {
+--    -- REQUIRED - you must specify a snippet engine
+--    expand = function(args)
+--      require('luasnip').lsp_expand(args.body)
+--    end,
+--  },
+--  window = {
+--    completion = cmp.config.window.bordered(),
+--    documentation = cmp.config.window.bordered(),
+--  },
+--  mapping = cmp.mapping.preset.insert({
+--    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+--    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+--    ['<C-Space>'] = cmp.mapping.complete(),
+--    ['<C-e>'] = cmp.mapping.abort(),
+--    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+--  }),
+--  sources = cmp.config.sources({
+--    { name = 'luasnip' }
+--  }, {
+--    { name = 'buffer' },
+--  })
+--})
+
+--require("lspconfig").lua_ls.setup { capabilities = capabilities }
+----require("lspconfig").tsserver.setup { capabilities = capabilities }
+--require("lspconfig").tsserver.setup { capabilities = capabilities }
+--
+--require("lspconfig").pylsp.setup { capabilities = capabilities }
+--require("lspconfig").dockerls.setup { capabilities = capablities }
+--require("lspconfig").docker_compose_language_service.setup { capabilities = capablities }
+--require("lspconfig").marksman.setup { capabilities = capablities }
+--require("lspconfig").sqlls.setup { capabilities = capablities }
+--require("lspconfig").bashls.setup { capabilities = capablities }
+--require("lspconfig").jsonls.setup { capabilities = capablities }
 
 --require('lint').linters_by_ft = {
 --  markdown = { 'vale' },
--- nvim/lua/plug.lua
+++ nvim/lua/plug.lua
@@ -58,6 +58,7 @@ return require('packer').startup(function(use)
   use {
     'hrsh7th/cmp-nvim-lsp',
     'hrsh7th/nvim-cmp',
+    'saadparwaiz1/cmp_luasnip',
     --'hrsh7th/cmp-buffer',
     --'hrsh7th/cmp-path',
     --'hrsh7th/cmp-cmdline',
