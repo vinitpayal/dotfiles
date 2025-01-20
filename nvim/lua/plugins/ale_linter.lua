b019c1c7b47cfbd4a4a74a3f96ed83b1fa8892b4
vinit payal <vinitpayal@gmail.com>
2025-01-20 18:24:20 +0530

    Correct auto linting

--- nvim/lua/plugins/ale_linter.lua
+++ nvim/lua/plugins/ale_linter.lua
@@ -4,7 +4,7 @@ return {
       -- Configuration goes here.
       local g = vim.g
 
-      -- g.ale_ruby_rubocop_auto_correct_all = 1
+      g.ale_ruby_rubocop_auto_correct_all = 1
       g.ale_fix_on_save = 1
       g.ale_sign_error = '❌'
       g.ale_sign_warning = '⚠️'
