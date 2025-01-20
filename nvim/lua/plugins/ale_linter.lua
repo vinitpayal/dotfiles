01fefcf5d7cc2ae19df2c7c202394fc62ac36744
vinit payal <vinitpayal@gmail.com>
2025-01-20 18:23:57 +0530

    ale linter enable fix on save for auto formatting

--- nvim/lua/plugins/ale_linter.lua
+++ nvim/lua/plugins/ale_linter.lua
@@ -5,7 +5,7 @@ return {
       local g = vim.g
 
       -- g.ale_ruby_rubocop_auto_correct_all = 1
-      -- g.ale_fix_on_save = 1
+      g.ale_fix_on_save = 1
       g.ale_sign_error = '❌'
       g.ale_sign_warning = '⚠️'
 
