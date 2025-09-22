74ee4e0cb9249d661aa57f518bf1897fb1e7e152
vinit payal <vinitpayal@gmail.com>
2025-09-22 22:39:50 +0530

    Avante add markdown and image plugin

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -19,7 +19,7 @@ return {
     behaviour = {
       auto_suggestions = true, -- Experimental stage
       auto_set_highlight_group = true,
-      auto_set_keymaps = true, -- Disable to avoid conflicts
+      auto_set_keymaps = true, -- Disabled to avoid conflicts
       auto_apply_diff_after_generation = false,
       support_paste_from_clipboard = true,
       minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
@@ -33,13 +33,30 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
-    -- {
-    --   -- Make sure to set this up properly if you have lazy=true
-    --   'MeanderingProgrammer/render-markdown.nvim',
-    --   opts = {
-    --     file_types = { "markdown", "Avante" },
-    --   },
-    --   ft = { "markdown", "Avante" },
-    -- },
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
   },
 }
