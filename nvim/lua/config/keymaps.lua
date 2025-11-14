00d6995cdbb30db293dc9959821ee4af60c9537d
vinit payal <vinitpayal@gmail.com>
2025-11-14 10:31:01 +0530

    neovim copy file path + tmux pane switching

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Copy relative file path with line number
+vim.keymap.set("n", "<leader>cf", function()
+  local filepath = vim.fn.expand("%:.")
+  local line_number = vim.fn.line(".")
+  local reference = filepath .. ":" .. line_number
+  vim.fn.setreg("+", reference)
+  vim.notify('Copied: ' .. reference, vim.log.levels.INFO)
+end, { desc = "Copy file path with line number" })
